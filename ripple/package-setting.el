485380000dd5f439539103d5f9c875f741b2b8b2
ripple <ripple0328@gmail.com>
2012-07-09 16:58:03 +0800

    add some packages

--- ripple/package-setting.el
+++ ripple/package-setting.el
@@ -1,9 +1,14 @@
+;;-------------------------------------------------------------------------------
 ;; change a repository for elpa
+;;------------------------------------------------------------------------------
+
 (require 'package)
 (setq package-archives (cons '("tromey" . "http://tromey.com/elpa/") package-archives))
 (package-initialize)
 
+;;------------------------------------------------------------------------------
 ;; check and install el-get
+;;------------------------------------------------------------------------------
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get") 
 
@@ -12,49 +17,53 @@
   (with-current-buffer 
       (url-retrieve-synchronously "https://raw.github.com/dimitri/el-get/master/el-get-install.el") 
     (goto-char (point-max)) 
-
     (eval-print-last-sexp)))
+
+;;------------------------------------------------------------------------------
+;; packages sources
+;;------------------------------------------------------------------------------
 
 (setq el-get-sources 
       '(
-	anything
-	auto-complete
-	auto-complete-css
-	auto-complete-emacs-lisp
-	auto-complete-extension
-	auto-complete-ruby
-	auto-complete-yasnippet
-	babel
-	coffee-mode
-	el-get
-	emacschrome
-	emacsserver
-	flymake-ruby
-	inf-ruby
-	package
-	rdebug
-	ri-emacs
-	rinari
-	rspec-mode
-	ruby-block
-	ruby-comilation
-	ruby-electric
-	ruby-end
-	ruby-mode
-	ruby-test
-	rvm
-	scss-mode
-	xcode
-	itunes
-  skype
-  slime
-	)
-)
-
-
-
-
-(el-get 'sync)
+        anything
+        auto-complete
+        auto-complete-css
+        auto-complete-emacs-lisp
+        auto-complete-extension
+        auto-complete-ruby
+        auto-complete-yasnippet
+        babel
+        coffee-mode
+        css-mode
+        dired+
+        el-get
+        emacschrome
+        emacsserver
+        flymake-ruby
+        inf-ruby
+        magit
+        o-blog
+        package
+        rdebug
+        ri-emacs
+        rinari
+        rspec-mode
+        ruby-block
+        ruby-comilation
+        ruby-electric
+        ruby-end
+        ruby-mode
+        ruby-test
+        rvm
+        scss-mode
+        xcode
+        itunes
+        skype
+        autopair
+        yasnippet
+        zencoding-mode
+        ))
+(el-get 'wait)
 
 
 (provide 'package-setting)
