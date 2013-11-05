2596e8b28cdfda2474975d77d5fc87166bd07157
Qingbo ZHANG <ripple0328@gmail.com>
2013-11-05 18:20:42 +0800

    add echo message to install script

--- install.sh
+++ install.sh
@@ -1,4 +1,23 @@
+function msg {
+ echo "###"$1
+}
+
+msg "updating homebrew"
+brew update
+
+msg "installing emacs"
 brew install emacs --HEAD --use-git-head --cocoa --srgb
+
+msg "linking installed emacs to /Application"
 ln -s /usr/local/Cellar/emacs/HEAD/Emacs.app /Applications
-gem install rubocop pry pry-doc method_source				 
-brew install the_silver_searcher ispell gpg
+
+msg "installing some emacs package dependencies"
+msg "installing rubocop pry method_source"
+gem install rubocop pry pry-doc method_source
+
+msg "installing fontpatcher for powlinbe"
+brew install --use-gcc fontforge
+
+msg "installing silver searcher ispell csv gpg and so on"
+brew tap epichub/homebrew-epicbrews 
+brew install the_silver_searcher ispell gpg cvs
