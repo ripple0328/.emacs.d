cbc771376f4d16d38f6c4f2bede3456d3fc0f1d6
Qingbo ZHANG <ripple0328@gmail.com>
2013-11-05 18:20:56 +0800

    change emacsclient execable location for magit

--- el-get-init-files/init-magit.el
+++ el-get-init-files/init-magit.el
@@ -2,4 +2,4 @@
 
 ;; magit-emacsclient-executable looks emacsclient in /usr/bin/emacsclient that was
 ;; not related to emacs for osx, so I customize it
-(setq magit-emacsclient-executable (concat invocation-directory "bin/emacsclient"))
+;; (setq magit-emacsclient-executable (concat invocation-directory "bin/emacsclient"))
