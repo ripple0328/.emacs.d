109e9195bfcace3f790a60193a1230e78fe5c6c5
Qingbo <qbzhang@thoughtworks.com>
2013-11-13 18:43:37 +0800

    change keybinding of ace-jump-mode

--- el-get-init-files/init-ace-jump-mode.el
+++ el-get-init-files/init-ace-jump-mode.el
@@ -1,3 +1,5 @@
-(global-set-key (kbd "C-c SPC") 'ace-jump-word-mode)
-;; (global-set-key (kbd "C-u C-c SPC") ' ace-jump-char-mode)
-;; (global-set-key (kbd "C-u C-u C-c SPC") ' ace-jump-line-mode)
+(global-set-key (kbd "C-c SPC") 'ace-jump-mode)
+(setq ace-jump-mode-submode-list
+      '(ace-jump-word-mode              ;; the first one always map to : C-c SPC
+        ace-jump-line-mode           ;; the third one always map to ：C-u C-u C-c SPC
+        ace-jump-char-mode))              ;; the second one always map to: C-u C-c SPC            
