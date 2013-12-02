567da4871c7c7849088ac6a2d7547d89196d54de
Qingbo <ripple0328@gmail.com>
2013-12-02 21:17:20 +0800

    get first element of the shell name by car

--- configuration/shell-configuration.el
+++ configuration/shell-configuration.el
@@ -1,5 +1,7 @@
-(setq shell-file-name (getenv "SHELL"))
-(setq explicit-shell-file-name (concat "/bin/" (getenv "SHELL")))
+(setq shell-file-name (car (last (split-string (getenv "SHELL") "/"))))
+(setq explicit-shell-file-name (getenv "SHELL"))
+(setenv "TERM" "xterm-256color")
+(setenv "SHELL" shell-file-name)
 (global-set-key "\C-cs" 'shell)
 
 ;; removing ^M character
