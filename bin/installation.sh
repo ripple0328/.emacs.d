467da34db7f1ab815f3e5fe52fe4fca38a4e8b11
Qingbo <ripple0328@gmail.com>
2013-12-14 00:32:13 +0800

    add automatic online install script

---
+++ bin/installation.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+cd $HOME
+curl -L https://raw.github.com/ripple0328/.emacs.d/master/bin/install-utils | sh
+
+# add dependencies
+install-brew
+check-and-install-git
+check-and-brew-install hub
+
+# git all emacs configuration code
+mv .emacs.d .emacs.d.bak
+hub clone ripple0328/.emacs.d
+
+# prepare environment and install emacs
+clean-env
+install-emacs
+create-command-line-emacs
+daemon-emacs
+
+# install dependencies
+curl -L https://raw.github.com/ripple0328/.emacs.d/master/bin/install-dependencies | sh
+
+patch-shebang-path $(emacs-path) ruby
+msg 'First time you launch emacs, all plugin will install automatically. enjoy!' 
+emacs --debug-init
+
