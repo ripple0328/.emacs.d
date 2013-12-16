e9b4c562de6d975eff536bbb536e85ddf93cf516
Qingbo <ripple0328@gmail.com>
2013-12-16 19:44:14 +0800

    add exec-local and exec-remote function to installation script

--- bin/installation.sh
+++ bin/installation.sh
@@ -1,30 +1,21 @@
 #!/usr/bin/env bash
 
-# get permisson at beginning
-sudo ls > /dev/null 2>&-
-
 cd $HOME
-curl -L https://raw.github.com/ripple0328/.emacs.d/master/bin/install-utils | sh
 
-# add dependencies
-install-brew
-check-and-install-git
-check-and-brew-install hub
+function exec-remote {
+    curl -L https://raw.github.com/ripple0328/.emacs.d/master/bin/$1 | sh
+}
 
-# git all emacs configuration code
+exec-remote install-utils
+exec-remote env-prepare
+
+# back up old configuration and clone new one
 mv .emacs.d .emacs.d.bak
 hub clone ripple0328/.emacs.d
 
-# prepare environment and install emacs
-clean-env
-install-emacs
-create-command-line-emacs
-daemon-emacs
+exec-remote install-process
 
 # install dependencies
-curl -L https://raw.github.com/ripple0328/.emacs.d/master/bin/install-dependencies | sh
+exec-remote install-dependencies
 
-patch-shebang-path $(emacs-path) ruby
-msg 'First time you launch emacs, all plugin will install automatically. enjoy!' 
-emacs --debug-init
 
