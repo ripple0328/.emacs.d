7402c0ebb27c251ab0a97655e22c912dde5be566
Qingbo <ripple0328@gmail.com>
2014-01-04 00:28:40 +0800

    cd .emacs.d after clone it

--- bin/install.sh
+++ bin/install.sh
@@ -8,8 +8,6 @@ function exec-remote {
   source /dev/stdin <<< "$(curl -sSL https://raw.github.com/ripple0328/.emacs.d/master/bin/$1)"
 }
 
-exec-remote emacs-utils
-exec-remote emacs-install-process
 
 # back up old configuration and clone new one
 if [ -f .emacs.d ]; then
@@ -18,7 +16,11 @@ fi
 
 if [ ! -f .emacs.d ]; then
     git clone https://github.com/ripple0328/.emacs.d.git
+    cd .emacs.d
 fi
+
+exec-remote emacs-utils
+exec-remote emacs-install-process
 
 # install emacs dependencies
 exec-remote install-dependencies
