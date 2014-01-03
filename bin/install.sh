61bc0bdebc611aa41a4c8069685af0c48febd7f1
Qingbo <ripple0328@gmail.com>
2014-01-04 00:26:11 +0800

    change order of installation

--- bin/install.sh
+++ bin/install.sh
@@ -8,6 +8,8 @@ function exec-remote {
   source /dev/stdin <<< "$(curl -sSL https://raw.github.com/ripple0328/.emacs.d/master/bin/$1)"
 }
 
+exec-remote emacs-utils
+exec-remote emacs-install-process
 
 # back up old configuration and clone new one
 if [ -f .emacs.d ]; then
@@ -17,9 +19,6 @@ fi
 if [ ! -f .emacs.d ]; then
     git clone https://github.com/ripple0328/.emacs.d.git
 fi
-
-exec-remote emacs-utils
-exec-remote emacs-install-process
 
 # install emacs dependencies
 exec-remote install-dependencies
