22f63ca9052bf7b761d1ffaab547f40600033ac3
Qingbo <ripple0328@gmail.com>
2014-09-27 04:25:51 +1000

    hidden useless mode in modeline

--- el-get-init-files/init-powerline.el
+++ el-get-init-files/init-powerline.el
@@ -10,3 +10,61 @@
 
 ;; (setq powerline-color1 "black")
 ;; (setq powerline-color2 "white")
+
+
+(defvar mode-line-cleaner-alist
+  `((auto-complete-mode . " α")
+    (yas-minor-mode . " γ")
+    (paredit-mode . " Φ")
+    (eldoc-mode . "")
+    (abbrev-mode . "")
+    (textmate-mode . "")
+    (visual-line-mode . "")
+    (smartparens-mode . "")
+    (flyspell-mode . "")
+    (undo-tree-mode . " τ")
+    (wrap-region-mode . "")
+    (volatile-highlights-mode . " υ")
+    (elisp-slime-nav-mode . " δ")
+    (nrepl-interaction-mode . " ηζ")
+    (auto-fill-function . " φ")
+    (autopair-mode . "")
+    (lambda-mode . "")
+    (kibit-mode . " κ")    
+    (robe-mode . "")
+
+    ;; Major modes
+    (nrepl-mode . "ηζ")
+    (clojure-mode . "λ")
+    (hi-lock-mode . "")
+    (python-mode . "Py")
+    (emacs-lisp-mode . "el")
+    (markdown-mode . "md")
+    (org-mode . "Ο")
+    (enh-ruby-mode . "Rb")
+    (rubocop-mode . "")
+    (dired-mode . "Dired")
+    (cperl-mode . "Pl")
+)
+  "Alist for `clean-mode-line'.
+
+When you add a new element to the alist, keep in mind that you
+must pass the correct minor/major mode symbol and a string you
+want to use in the modeline *in lieu of* the original.")
+
+
+(defun clean-mode-line ()
+  (interactive)
+  (loop for cleaner in mode-line-cleaner-alist
+        do (let* ((mode (car cleaner))
+                 (mode-str (cdr cleaner))
+                 (old-mode-str (cdr (assq mode minor-mode-alist))))
+             (when old-mode-str
+                 (setcar old-mode-str mode-str))
+               ;; major mode
+             (when (eq mode major-mode)
+               (setq mode-name mode-str)))))
+
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
+
