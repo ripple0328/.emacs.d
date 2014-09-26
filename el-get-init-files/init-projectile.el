2a1c3e74392ff2e81256e6fd9d5454be0b74bbca
Qingbo <ripple0328@gmail.com>
2014-09-27 04:26:20 +1000

    move configuration from projectile-rails to projectile

---
+++ el-get-init-files/init-projectile.el
@@ -0,0 +1,6 @@
+(projectile-global-mode)
+(setq projectile-enable-caching t)
+(setq projectile-completion-system 'grizzl)
+(setq projectile-enable-idle-timer t)
+(add-hook 'projectile-idle-timer-hook 'projectile-regenerate-tags)
+(add-hook 'projectile-mode-hook 'projectile-rails-on)
