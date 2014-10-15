409306263480da5bd31ee445d6b1a516f003b1ce
Qingbo <ripple0328@gmail.com>
2014-10-15 21:32:37 +0800

    add hooks to prog-mode for ggtags mode

--- configuration/program-setting.el
+++ configuration/program-setting.el
@@ -1,5 +1,5 @@
 (add-hook 'prog-mode-hook
           (lambda ()
-            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'cperl-mode-hook)
-              (ggtags-mode t))))
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'cperl-mode-hook 'perl-mode-hook)
+              (ggtags-mode))))
 (provide 'program-setting)
