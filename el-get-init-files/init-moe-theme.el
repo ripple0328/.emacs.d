46e3522980c56d660c2f5c59c740fe6c55d5396e
Qingbo <ripple0328@gmail.com>
2014-10-22 22:26:56 +0800

    change moe configuration

--- el-get-init-files/init-moe-theme.el
+++ el-get-init-files/init-moe-theme.el
@@ -1,16 +1,14 @@
+(require 'moe-theme)
 (require 'moe-theme-switcher)
 
 (setq moe-light-pure-white-background-in-terminal t)
-(setq moe-theme-resize-markdown-title '(2.0 1.7 1.5 1.3 1.0 1.0))
-(setq moe-theme-resize-org-title '(2.2 1.8 1.6 1.4 1.2 1.0 1.0 1.0 1.0))
-(setq moe-theme-resize-rst-title '(2.0 1.7 1.5 1.3 1.1 1.0))
-;; (Available colors: blue, orange, green ,magenta, yellow, purple, red, cyan, w/b.)
-(moe-theme-random-color)
+;; (available colors: blue, orange, green ,magenta, yellow, purple, red, cyan, w/b.)
 ;; (moe-theme-set-color 'orange)
 
+(moe-theme-random-color)
 
 (powerline-moe-theme)
 
 ;; 设置经纬度，好切换theme
-(setq calendar-latitude +34)
-(setq calendar-longitude +108)
+(setq calendar-latitude +34.26)
+(setq calendar-longitude +108.96)
