9c5fe5d312ab97e0020163f07e9aaabe37987c04
Qingbo <ripple0328@gmail.com>
2014-10-22 14:05:37 +0800

    add database configuration
    http://truongtx.me/2014/08/23/setup-emacs-as-an-sql-database-client/

--- configuration/program-setting.el
+++ configuration/program-setting.el
@@ -4,4 +4,36 @@
               (ggtags-mode))))
 (subword-mode)
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+
+
+;; database
+(add-hook 'sql-interactive-mode-hook
+          (lambda ()
+            (toggle-truncate-lines t)))
+
+;; database connection configuration
+(setq sql-connection-alist
+      '((local (sql-product 'mysql)
+                  (sql-server "localhost")
+                  (sql-user "root")
+                  (sql-database "PSeeker"))
+        (sf2 (sql-product 'mysql)
+                  (sql-server "pseeker.sf2.vpc.realestate.com.au")
+                  (sql-user "root")
+                  (sql-password "s3cr3t")
+                  (sql-database "PSeeker"))))
+
+(defun my-sql-connect (product connection)
+  ;; remember to set the sql-product, otherwise, it will fail for the first time
+  ;; you call the function
+  (setq sql-product product)
+  (sql-connect connection))
+(defun my-sql-local ()
+  (interactive)
+  (my-sql-connect 'mysql 'local))
+
+(defun my-sql-sf2 ()
+  (interactive)
+  (my-sql-connect 'mysql 'sf2))
+
 (provide 'program-setting)
