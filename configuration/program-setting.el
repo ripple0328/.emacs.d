a8a7408270c0d8f13b50e44049f86edb9a705699
Qingbo <ripple0328@gmail.com>
2014-10-29 17:43:42 +0800

    hood ggtags not only to perl but all prog-mode

--- configuration/program-setting.el
+++ configuration/program-setting.el
@@ -1,7 +1,8 @@
 (add-hook 'prog-mode-hook
           (lambda ()
-            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'cperl-mode-hook 'perl-mode-hook 'ruby-mode-hook 'enh-ruby-mode)
-              (ggtags-mode))
+            ;; (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'cperl-mode-hook 'perl-mode-hook 'ruby-mode-hook 'enh-ruby-mode)
+              (ggtags-mode)
+;; )
             (yas-minor-mode)
             (flyspell-prog-mode)
             (smartscan-mode)
