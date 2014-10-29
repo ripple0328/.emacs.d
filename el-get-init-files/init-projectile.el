9b49d6e0b837cf16a313b8527e86667e9b809873
Qingbo <ripple0328@gmail.com>
2014-10-29 21:21:59 +0800

    disable projectile cache to avoid not found files

--- el-get-init-files/init-projectile.el
+++ el-get-init-files/init-projectile.el
@@ -2,5 +2,6 @@
 (setq projectile-enable-caching t)
 (setq projectile-completion-system 'grizzl)
 (setq projectile-enable-idle-timer t)
+(setq projectile-enable-caching nil)
 (add-hook 'projectile-idle-timer-hook 'projectile-regenerate-tags)
 
