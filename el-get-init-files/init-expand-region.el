0b77aaf464657bb1717f0b7c845ac2f21e63a831
Qingbo <ripple0328@gmail.com>
2014-12-18 22:43:10 +0800

    add keybinding for expand-region to support more expand mode

--- el-get-init-files/init-expand-region.el
+++ el-get-init-files/init-expand-region.el
@@ -1,1 +1,8 @@
 (global-set-key (kbd "C-=") 'er/expand-region)
+(global-set-key (kbd "C-c ' = =") 'er/expand-region)
+(global-set-key (kbd "C-c ' = d") 'er/mark-defun)
+(global-set-key (kbd "C-c ' = u") 'er/mark-url)
+(global-set-key (kbd "C-c ' = w") 'er/mark-word)
+(global-set-key (kbd "C-c ' = e") 'er/mark-email)
+(global-set-key (kbd "C-c ' = s") 'er/mark-symbol)
+(global-set-key (kbd "C-c ' = c") 'er/mark-comment)
