e078bca26ce54904c4c89c82032cba27c6f5be04
Qingbo <ripple0328@gmail.com>
2014-12-18 22:42:22 +0800

    Change keybinding of multi-cursors to avoid using of super key
    because on mac I always get C-s-xxxxx is undefined

--- el-get-init-files/init-multiple-cursors.el
+++ el-get-init-files/init-multiple-cursors.el
@@ -1,6 +1,7 @@
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-
-
+(global-set-key (kbd "C-&") 'mc/edit-lines)
+(global-set-key (kbd "C-c C-e") 'mc/edit-ends-of-lines)
+(global-set-key (kbd "C-c C-a") 'mc/edit-beginnings-of-lines)
+(global-set-key (kbd "C-*") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-'") 'mc-hide-unmatched-lines-mode)
